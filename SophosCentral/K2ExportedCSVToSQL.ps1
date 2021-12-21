8ad342788fe70f57f4b29d1d3858feb93be858aa
802-1x <96475907+802-1x@users.noreply.github.com>
2021-12-22 10:07:58 +1100

    Update K2ExportedCSVToSQL.ps1

--- SophosCentral/K2ExportedCSVToSQL.ps1
+++ SophosCentral/K2ExportedCSVToSQL.ps1
@@ -1,14 +1,18 @@
 # Process the Workstation and Server Reports from Sophos Central received from the automated email reports. The attachments are stripped out using K2 and exported to CSV.
 Import-Module dbatools
 
-$wkstationReport = Import-Csv -Path "\\server\download\K2\Sophos\Computers*.csv"
-$serverReport    = Import-Csv -Path "\\server\download\K2\Sophos\Servers*.csv"
-$updateTime      = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
-function processFiles {
+$wkstationFile          = Get-ChildItem "\\server\download\K2\Sophos\Computers*.csv" | Sort-Object LastWriteTime | Select-Object -Last 1
+$serverFile             = Get-ChildItem "\\server\download\K2\Sophos\Servers*.csv"   | Sort-Object LastWriteTime | Select-Object -Last 1
+$script:wkstationReport = Import-Csv $wkstationFile
+$script:serverReport    = Import-Csv $serverFile
+$script:updateTime      = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+$log                    = "\\server\download\K2\Sophos\log\log.txt"
+
+function processReport {
     # Process the workstations and add a workstation type flag
-    $wkstationArray=@()
+    $script:wkstationArray=@()
     foreach ($wkstation in $wkstationReport) {
-        $wkstationArray += [PSCustomObject]@{
+        $script:wkstationArray += [PSCustomObject]@{
             Name                    = $wkstation.Name;
             Online                  = $wkstation.Online;
             LastUser                = $wkstation.'Real-time Scan';
@@ -20,15 +24,15 @@ function processFiles {
             EncryptionStatus        = $wkstation.'Encryption Status';
             HealthStatus            = $wkstation.'Health Status';
             Group                   = $wkstation.Group;
-            Type                    = 'Wokstation';
+            Type                    = 'Workstation';
             UpdateTime              = $updateTime
         }
     }
 
     # Process the servers and add a server type flag
-    $serverArray=@()
+    $script:serverArray=@()
     foreach ($server in $serverReport) {
-        $serverArray += [PSCustomObject]@{
+        $script:serverArray += [PSCustomObject]@{
             Name                    = $server.Name;
             Online                  = $server.Online;
             LastUser                = $server.'Real-time Scan';
@@ -44,13 +48,16 @@ function processFiles {
             UpdateTime              = $updateTime
         }
     }
-}
-
- 
-if($null -ne $wkstationReport -and $null -ne $serverReport){
-    processFiles
+} 
+# Check for available update file, condense reports and update SQL, add log entry
+if($null -ne $serverReport -and $null -ne $wkstationReport){
+    processReport
     $totalReport = $wkstationArray + $serverArray
-    $clearTable  = Invoke-DbaQuery      -SqlInstance "serverfqdn" -Database Sophos -Query "DELETE FROM dbo.CentralReport"
-    $updateTable = Write-DbaDbTableData -SqlInstance "serverfqdn" -Database Sophos -Table dbo.CentralReport -InputObject $totalReport
+    Invoke-DbaQuery      -SqlInstance "ServerFQDN" -Database Sophos -Query "DELETE FROM dbo.CentralReport"
+    Write-DbaDbTableData -SqlInstance "ServerFQDN" -Database Sophos -Table dbo.CentralReport -InputObject $totalReport
+    $wkstationReportFilename = Get-ChildItem -Path "\\server\download\K2\Sophos\Computers*.csv"
+    $serverReportFilename    = Get-ChildItem -Path "\\server\download\K2\Sophos\Servers*.csv"
+    "$updateTime Wrote $wkstationReportFilename.Name" | Out-File $log -Append
+    "$updateTime Wrote $serverReportFilename.Name"    | Out-File $log -Append
     Remove-Item -Path "\\server\download\K2\Sophos\*.*" -Force
 }
