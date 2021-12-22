a584d410feb3e75f864ee2ba3d62026a66a42af1
802-1x <96475907+802-1x@users.noreply.github.com>
2021-12-23 09:13:32 +1100

    Create ProcessAgedComputers.ps1

---
+++ AD/ProcessAgedComputers.ps1
@@ -0,0 +1,33 @@
+
+$CurrentDate = get-date
+$Days = -61 #AD password renewal date and SCCM Databse retention period
+$DaysToDelete = -91 #
+
+$LogDate = Get-Date -format yyyyMMddhhmm
+$ScriptLogPath = $PsScriptRoot+"\Script Log\"
+$ResultsLogPath = $PsScriptRoot+"\Results\"
+
+Start-Transcript $ScriptLogPath\$LogDate"_AgedComputersScript.log"
+
+Import-Module ActiveDirectory
+
+Write-Host "Current Date = $CurrentDate"
+Write-Host "Days Aged = $Days"
+Write-Host "Days to Delete = $DaysToDelete"
+
+
+#Disables and moves aged Computer objects from the WK-Resources OU
+$LastLogonDateCheck = $CurrentDate.AddDays($Days)
+$AgedComputers = Get-ADComputer -SearchBase "OU"  -Filter {LastLogon -lt $LastLogonDateCheck -and Enabled -eq $true} -Properties Name,Description,lastlogon,distinguishedName,memberof | `
+                 tee-object -file $ResultsLogPath$LogDate"_AgedComputers.log" | `
+                 Move-ADObject -targetpath "OU" -PassThru | `
+                 Set-ADComputer -Enabled $false
+#This returns the "lastlogon" as [fileTime] need to use [datetime]::FromFileTime("*ValueAsInt*") to convert it back
+
+#Deletes Old Computer objects from the Computers for Deletions OU
+$WhenDisabledLogonDateCheck = $CurrentDate.AddDays($DaysToDelete)
+$DeleteComputers = Get-ADComputer -SearchBase "OU" -Filter {LastLogon -lt $WhenDisabledLogonDateCheck -and Enabled -eq $false} -Properties Name,Description,lastlogon,distinguishedName,memberof,useraccountcontrol | `
+                 Tee-object -file $ResultsLogPath$LogDate"_DeletedComputers.log" | `
+                 Remove-ADComputer -Confirm:$false
+
+Stop-Transcript
