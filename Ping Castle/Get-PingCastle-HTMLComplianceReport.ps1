7ffe4c089c82108e217f1c203327f50ac8d15a0a
802-1x <96475907+802-1x@users.noreply.github.com>
2023-11-14 22:02:26 +1100

    Update Get-PingCastle-HTMLComplianceReport.ps1

--- Ping Castle/Get-PingCastle-HTMLComplianceReport.ps1
+++ Ping Castle/Get-PingCastle-HTMLComplianceReport.ps1
@@ -1,6 +1,35 @@
-#WIP
 
+
+
+$url = "https://github.com/vletoux/pingcastle/releases/latest"
+
+# Send a GET request to the URL
+$response = Invoke-WebRequest -Uri $url
+
+# Extract the URL of the latest release
+$latestReleaseUrl = $response.BaseResponse.ResponseUri.AbsoluteUri
+
+# Display the captured URL
+Write-Output "Latest Release URL: $latestReleaseUrl"
+
+# Truncate the text after the last /
+$latestReleaseName = (Split-Path $latestReleaseUrl -Leaf)
+
+# Display the captured release name
+Write-Output "Latest Release Name: $latestReleaseName"
+
+
+
+
+#Generate report files.
 $fileExecutable = "C:\Users\<redacted>\Downloads\PingCastle_2.10.1.1\PingCastle.exe"
-
 Start-Process -NoNewWindow -FilePath "$fileExecutable" -ArgumentList "--healthcheck --server <redacted>"
 
+# Load XML from file for analysis
+$xml = [xml](Get-Content -Path "C:\Users\<redacted>\Downloads\PingCastle_2.10.1.1\<redacted>.xml")
+
+$xml.HealthCheckData.GlobalScore
+$xml.EngineVersion
+$xml.
+
+$latestReleaseName
